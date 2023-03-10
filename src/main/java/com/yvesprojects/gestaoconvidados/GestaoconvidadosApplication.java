974ef0ce6ba00cc9cad9b67cd89fbbeb3b60df8a
yvesls <yveslimasilva@gmail.com>
2023-03-10 14:04:02 -0300

    conectando o projeto ao banco de dados

---
+++ src/main/java/com/yvesprojects/gestaoconvidados/GestaoconvidadosApplication.java
@@ -0,0 +1,13 @@
+package com.yvesprojects.gestaoconvidados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoconvidadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoconvidadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yvesprojects/gestaoconvidados/GestaoconvidadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvesprojects.gestaoconvidados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoconvidadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
